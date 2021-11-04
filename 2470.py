400b545666c3b51f9dd4a32b1143b7f73ead931c
lokihyeon <hae8573@koreatech.ac.kr>
2021-11-05 01:12:55 +0900

    Two Point Algorithm by baekjon

---
+++ 2470.py
@@ -0,0 +1,22 @@
+import sys
+
+n = int(input())
+arr = sorted(list(map(int, sys.stdin.readline().split())))
+
+arr.sort()
+start, end =0, n-1
+ansStart, ansEnd =0, 0
+min = sys.maxsize
+
+while start < end:
+    tmp = arr[start] + arr[end]
+    if(min > abs(tmp)):
+        min = abs(tmp)
+        ansStart = arr[start]
+        ansEnd = arr[end]
+
+    elif tmp <= 0:
+        start +=1
+    else: end -=1
+
+print(ansStart, ansEnd)
