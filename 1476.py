94e48dd11b865b9dd57f81d558b8d276f63931b9
lokihyeon <hae8573@koreatech.ac.kr>
2021-11-11 16:28:50 +0900

    re

---
+++ 1476.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+E, S, M = map(int, input().split())
+
+year = 0
+e, s, m = 0, 0, 0
+while True:
+    year += 1
+    e = (e + 1) % 16 if (e + 1) % 16 !=0 else 1
+    s = (s + 1) % 29 if (s + 1) % 29 !=0 else 1
+    m = (m + 1) % 20 if (m + 1) % 20 !=0 else 1
+    if e == E and s == S and m == M:
+        print(year)
